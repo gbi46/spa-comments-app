2f8a2ec0f256535a8f347b94d321bfbd979efe4d
Your Name <you@example.com>
2025-03-07 11:40:26 +0100

    add file field to form

--- comments/forms.py
+++ comments/forms.py
@@ -2,20 +2,21 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
-    user_name = forms.CharField(max_length=100, required=True, label="User Name")
+    user_name = forms.CharField(max_length=100, required=True, label="Пользователь")
     email = forms.EmailField(required=True, label="E-mail")
-    home_page = forms.URLField(required=False, label="Home Page")
+    home_page = forms.URLField(required=False, label="Веб страница")
+    file = forms.FileField(required=False, label="Файл")
     text = forms.CharField(widget=forms.Textarea, required=True, label="Комментарий")
     captcha = forms.CharField(max_length=6, required=True, label="Введите код с картинки")
 
     class Meta:
         model = Comment
-        fields = ['text', 'user_name', 'email', 'home_page', 'captcha', 'parent']
+        fields = ['file', 'text', 'user_name', 'email', 'home_page', 'captcha', 'parent']
 
     def clean_user_name(self):
         user_name = self.cleaned_data.get('user_name')
         if not user_name.isalnum():
-            raise forms.ValidationError("User Name может содержать только латинские буквы и цифры.")
+            raise forms.ValidationError("Имя пользователя может содержать только латинские буквы и цифры.")
         return user_name
 
     def clean_captcha(self):
